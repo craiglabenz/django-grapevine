5fbf8082de45f75e7b08ca9ceeb7ca4c3af2c6e6
Craig Labenz <craig.labenz@gmail.com>
2014-09-19 17:31:09 -0400

    Configures installation to include nested directories
    Some call this "important"
    Bumps to 0.1.0-rc.3

--- setup.py
+++ setup.py
@@ -1,7 +1,58 @@
 import os
+import re
+import sys
 from setuptools import setup
 
-from grapevine import __version__
+
+# Borrowed from the infamous
+# https://github.com/tomchristie/django-rest-framework/blob/master/setup.py
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+# Borrowed from the infamous
+# https://github.com/tomchristie/django-rest-framework/blob/master/setup.py
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+# Borrowed from the infamous
+# https://github.com/tomchristie/django-rest-framework/blob/master/setup.py
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
+version = get_version('grapevine')
+
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    os.system("python setup.py bdist_wheel upload")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
+    sys.exit()
 
 
 CLASSIFIERS = [
@@ -18,11 +69,12 @@ INSTALL_REQUIRES = [
 
 setup(
     name="django-grapevine",
-    packages=["grapevine"],
-    version=__version__,
+    packages=get_packages("grapevine"),
+    package_data=get_package_data("grapevine"),
+    version=version,
     description="A comprehensive email tool for Django",
     url="https://github.com/craiglabenz/django-grapevine",
-    download_url="https://github.com/craiglabenz/django-grapevine/tarball/{0}".format(__version__),
+    download_url="https://github.com/craiglabenz/django-grapevine/tarball/{0}".format(version),
     keywords=["django", "email", "sending", "tracking"],
     classifiers=CLASSIFIERS,
     install_requires=INSTALL_REQUIRES,
