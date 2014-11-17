3ee12cdb8b46c19b8003a2fd2885677cd4e483ec
Craig Labenz <craig.labenz@gmail.com>
2014-11-16 20:25:47 -0500

    Bump to version 0.2.0

--- grapevine/__init__.py
+++ grapevine/__init__.py
@@ -3,6 +3,6 @@ from __future__ import unicode_literals
 """Package for Grapevine"""
 
 __project__ = "grapevine"
-__version__ = "0.1.0-rc.6"
+__version__ = "0.2.0"
 
 VERSION = __project__ + '-' + __version__
